ead8944ec3196f80ee7ffefb82e5b8eb4b4f1225
Alex C-G <alexcg@outlook.com>
2020-02-20 16:37:14 +1300

    Added share post function

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Write emails to console. not SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
