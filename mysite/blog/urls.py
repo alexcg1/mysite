2861f799d4f5d274b09ad4f46c1d3cbb0efef923
Alex C-G <alexcg@outlook.com>
2020-02-22 10:46:12 +1300

    RSS feed

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostsFeed
 from . import views
 
 app_name = 'blog'
@@ -10,5 +11,6 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag')
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
