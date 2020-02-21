880c06e84ac139177e1027d70c1dfb44ce00c053
Alex C-G <alexcg@outlook.com>
2020-02-21 14:41:10 +1300

    Added comments function

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
 from django.core.mail import send_mail
+
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -13,47 +14,30 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 1)  # 3 posts per page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-
-#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
-
-
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
 
+    # List of active comments
+    comments = post.comments.filter(active=True)
 
-# def post_share(request, post_id):
-#     # Retrieve post by id
-#     post = get_object_or_404(Post, id=post_id, status='published')
-#     sent = False
+    new_comment = None
 
-#     if request.method == 'POST':
-#         form = EmailPostForm(request.POST)
-#         # Form was submitted
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             post_url = request.build.absolute_uri(post.get_absolute_url())
-#             subject = '{} ({}) recommends you reading "{}"'.format(
-#                 cd['name'], cd['email'], post.title)
-#             message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(
-#                 post.title, post_url, cd['name'], cd['comments'])
-#             send_mail(subject, message, 'admin@myblog.com', 'foo@bar.com')
-#             sent = True
-#         else:
-#             form = EmailPostForm()
+    if request.method == 'POST':
+        # Comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create comment object
+            new_comment = comment_form.save(commit=False)
+            # Assign current post to comment
+            new_comment.post = post
+            # Save to db
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
 
-#         return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+
 
 def post_share(request, post_id):
     # Retrieve post by id
